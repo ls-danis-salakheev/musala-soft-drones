9c296171a39bda91fe6f8ccfd24b7d13f91fa6aa
Салахеев Данис Фанисович <dfsalakheev@sberbank.ru>
2022-11-11 21:50:57 +0600

    create enums: DroneModel and DroneState.

---
+++ src/main/java/com/musala/testdrones/danissalakheev/enums/DroneModel.java
@@ -0,0 +1,8 @@
+package com.musala.testdrones.danissalakheev.enums;
+
+public enum DroneModel {
+    Lightweight,
+    Middleweight,
+    Cruiserweight,
+    Heavyweight
+}
---
+++ src/main/java/com/musala/testdrones/danissalakheev/enums/DroneState.java
@@ -0,0 +1,12 @@
+package com.musala.testdrones.danissalakheev.enums;
+
+public enum DroneState {
+
+    IDLE,
+    LOADING,
+    LOADED,
+    DELIVERING,
+    DELIVERED,
+    RETURNING
+
+}
