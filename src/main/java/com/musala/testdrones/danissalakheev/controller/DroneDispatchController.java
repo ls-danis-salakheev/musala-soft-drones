60b8fbd87767fbf5119e084884be615e11a06a02
Салахеев Данис Фанисович <dfsalakheev@sberbank.ru>
2022-11-12 16:16:37 +0600

    refactor DroneDispatchController, remove pageable properties to new class Constants

--- src/main/java/com/musala/testdrones/danissalakheev/controller/DroneDispatchController.java
+++ src/main/java/com/musala/testdrones/danissalakheev/controller/DroneDispatchController.java
@@ -3,11 +3,17 @@ package com.musala.testdrones.danissalakheev.controller;
 import com.musala.testdrones.danissalakheev.dto.*;
 import com.musala.testdrones.danissalakheev.service.DroneService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
+
+import static com.musala.testdrones.danissalakheev.util.Constants.*;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,22 +28,27 @@ public class DroneDispatchController {
     }
 
     @GetMapping
-    public List<DroneOutDto> fetchAllDrones(@RequestParam(value = "size", defaultValue = "${app.pagination.defaultPage}") int page,
-                                            @RequestParam(value = "page", defaultValue = "${app.pagination.defaultSize}") int size) {
-        return droneService.getAll(page, size);
+    public List<DroneOutDto> fetchAllDrones(@RequestParam(required = false, defaultValue = DEFAULT_PAGE) int page,
+                                            @RequestParam(required = false, defaultValue = DEFAULT_SIZE) int size,
+                                            @RequestParam(required = false, defaultValue = DEFAULT_SORTING_DIRECTION) Sort.Direction direction,
+                                            @RequestParam(required = false, defaultValue = DRONE_DEFAULT_SORTING_PROPS) String[] properties) {
+        return droneService.getAll(PageRequest.of(page, size, direction, properties));
     }
 
     @GetMapping("/available")
-    public List<DroneInputDto> getAvailableDronesForLoad(@RequestParam(value = "page", defaultValue = "${app.pagination.defaultSize}") int page,
-                                                         @RequestParam(value = "size", defaultValue = "${app.pagination.defaultPage}") int size) {
-        return droneService.getAllAvailableForLoad();
+    public List<DroneInputDto> getAvailableDronesForLoad(@RequestParam(required = false, defaultValue = DEFAULT_PAGE) int page,
+                                                         @RequestParam(required = false, defaultValue = DEFAULT_SIZE) int size,
+                                                         @RequestParam(required = false, defaultValue = DEFAULT_SORTING_DIRECTION) Sort.Direction direction,
+                                                         @RequestParam(required = false, defaultValue = DRONE_DEFAULT_SORTING_PROPS) String[] properties) {
+        return droneService.getAllAvailableForLoad(PageRequest.of(page, size, direction, properties));
     }
 
-    @PutMapping( "/{serialNumber}")
-    public void loadDroneWithMedication(@PathVariable String serialNumber,
-                                                  @RequestPart MultipartFile image,
-                                                  @RequestPart @Valid MedicationDto medicationDto) {
+    @PutMapping("/{serialNumber}")
+    public ResponseEntity<Object> loadDroneWithMedication(@PathVariable String serialNumber,
+                                                          @RequestPart MultipartFile image,
+                                                          @RequestPart @Valid MedicationDto medicationDto) {
         droneService.loadWithMedications(serialNumber, medicationDto, image);
+        return new ResponseEntity<>(CREATED);
     }
 
     @GetMapping("/{serialNumber}/loaded")
---
+++ src/main/java/com/musala/testdrones/danissalakheev/util/Constants.java
@@ -0,0 +1,11 @@
+package com.musala.testdrones.danissalakheev.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+    public static final String DEFAULT_PAGE = "0";
+    public static final String DEFAULT_SIZE = "10";
+    public static final String DEFAULT_SORTING_DIRECTION = "ASC";
+    public static final String DRONE_DEFAULT_SORTING_PROPS = "serialNumber";
+}
